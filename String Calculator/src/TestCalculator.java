d653157a2e17fede99f79420af7c07aaa3143136
INDIA <INDIA@DESKTOP-FE2HGSI>
2021-07-21 22:30:27 +0530

    Fifth commit (refactoring @before annotation)

--- String Calculator/src/TestCalculator.java
+++ String Calculator/src/TestCalculator.java
@@ -3,12 +3,16 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.*;
 
 public class TestCalculator {
+	StringCalculator Calculator;
+
+	@Before
+	public void initialize() {
+		Calculator = new StringCalculator();
+	}
 
 	@Test
 	public void Test1() {
 //		test for blank string
-
-		StringCalculator Calculator = new StringCalculator();
 
 		assertEquals(0, Calculator.Add(""));
 	}
@@ -16,18 +20,12 @@ public class TestCalculator {
 	@Test
 	public void Test2() {
 //		test for a single number
-
-		StringCalculator Calculator = new StringCalculator();
-
 		assertEquals(13, Calculator.Add("13"));
 	}
-	
+
 	@Test
 	public void Test3() {
 //		test for a two numbers
-
-		StringCalculator Calculator = new StringCalculator();
-
 		assertEquals(3, Calculator.Add("1,2"));
 	}
 
